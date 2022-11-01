3d3207d745e988a1091b51a00d7f5356d9839525
CW <chiawei1@uw.edu>
2022-11-01 01:08:26 -0700

    Note: An updated version with a clear description.

--- Lab 4 - Flood Fill.py
+++ Lab 4 - Flood Fill.py
@@ -12,17 +12,16 @@ board = [
     "....##############....",
 ]
 
-myBoard = [[0]*22]*8
-myBoard[0] = list(board[0])
-myBoard[1] = list(board[1])
-myBoard[2] = list(board[2])
-myBoard[3] = list(board[3])
-myBoard[4] = list(board[4])
-myBoard[5] = list(board[5])
-myBoard[6] = list(board[6])
-myBoard[7] = list(board[7])
+
+# Step 1 - Since the board is an str object, assignment is not allowed. We want to convert it to a list type
+Board = []
+for x in range(len(board)):
+    Board = Board + [[0] * len(board[x])]
+for x in range(len(board)):
+    Board[x] = list(board[x])
 
 
+# Step 3 - The flood fill function. The function implemented the concept of recursion.
 def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
     """Returns board with old values replaced with new values
     through flood filling starting from the coordinates x, y
@@ -36,32 +35,31 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
         List[str]: Modified board
     """
 
-    # Implement your code here.
-
     if x >= 7 or x <= 0 or y >= 21 or y <= 0:
         return
-
-    if myBoard[x][y] == '~':
+    if Board[x][y] == '~':
+        return
+    if Board[x][y] == '#':
         return
 
-    if myBoard[x][y] == '#':
-        return
-
-    if myBoard[x][y] == old:
-        myBoard[x][y] = new
+    if Board[x][y] == old:
+        Board[x][y] = new
 
     flood_fill(input_board, old, new, x+1, y)
     flood_fill(input_board, old, new, x-1, y)
     flood_fill(input_board, old, new, x, y+1)
     flood_fill(input_board, old, new, x, y-1)
 
-    return myBoard
+    return Board
 
 
-modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
+# Step 2 - Execute the flood_fill function, and store the result into modified_board.
+modified_board = flood_fill(input_board=Board, old=".", new="~", x=5, y=12)
 
-for a in modified_board:
-    print(a)
+
+# Step 4 - Print modified_board as the form of an str object.
+for x in range(len(board)):
+    print(''.join(str(a) for a in modified_board[x]))
 
 # Expected output:
 # ......................
