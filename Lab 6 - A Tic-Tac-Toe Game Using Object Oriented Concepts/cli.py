e6559764a37b20104015e1d5843ce5e02d94abf1
CW <chiawei1@uw.edu>
2022-11-20 03:21:06 -0800

    Note: Updated

--- Lab 6 - A Tic-Tac-Toe Game Using Object Oriented Concepts/cli.py
+++ Lab 6 - A Tic-Tac-Toe Game Using Object Oriented Concepts/cli.py
@@ -1,1 +1,107 @@
+# This file contains the Command Line Interface (CLI) for
+# the Tic-Tac-Toe game. This is where input and output happens.
+# For core game logic, see logic.py.
 
+from logic import *
+import random
+
+if __name__ == '__main__':
+
+    board = Board()
+    winner = None
+    game_mode = None
+
+    while winner == None:
+
+        # TODO: Initialize the game.
+        if board.empty == True:
+
+            game_mode = input(
+                'Please enter the game mode number (1. You vs Bot; 2. I have friends): ')
+            while game_mode != '1' and game_mode != '2':
+                print('Invalid! Note: Please enter 1 or 2')
+                game_mode = input(
+                    'Please re-enter the game mode number (1. You vs Bot; 2. I have friends): ')
+
+            if game_mode == '1':
+                current_player = Player('O')
+                print('\nYou are \'O\'. Bot is \'X\'')
+            else:
+                Temp = input(
+                    'Who is going to be the first turn (\'O\' or \'X\')? ')
+                while Temp != 'O' and Temp != 'X':
+                    print('Invalid! Note: Please enter \'O\' or \'X\'')
+                    Temp = input(
+                        'Who is going to be the first turn (\'O\' or \'X\')? ')
+                current_player = Player(Temp)
+
+            print('Let\'s start the game. Now, it is',
+                  str(current_player.name), '\'s turn!')
+            print(board)
+
+        # TODO: Human vs Bot
+        if game_mode == '1':
+
+            if current_player.name == 'O':
+                # TODO: Input a move from the player.
+                X, Y = input(
+                    "Please enter your coordinates X Y (e.g., enter '0 1' for the coordinate (0,1)): ").split()
+                X = int(X)
+                Y = int(Y)
+                while (X != 0 and X != 1 and X != 2) or (Y != 0 and Y != 1 and Y != 2):
+                    print("Incorrect corrdinates entered!")
+                    X, Y = input(
+                        "Please re-enter your coordinates X Y: ").split()
+                    X = int(X)
+                    Y = int(Y)
+
+                # TODO: Update the board.
+                board.set(X, Y, current_player.name)
+
+            else:
+                X = random.randint(0, 2)
+                Y = random.randint(0, 2)
+                while board.get(X, Y) != None:
+                    X = random.randint(0, 2)
+                    Y = random.randint(0, 2)
+
+                board.set(X, Y, current_player.name)
+
+            if current_player.name == 'X':
+                print(board)
+                print("'X' just completed the action.")
+            else:
+                print(board)
+
+            # TODO: Update who is the next turn.
+            winner = board.get_winner()
+            if winner == None:
+                current_player.next_turn()
+                print("\nNow, it is", current_player.name, "'s turn!")
+            else:
+                print("\n" + winner, "just won the game.")
+
+        # TODO: Human vs Human
+        else:
+            # TODO: Input a move from the player.
+            X, Y = input(
+                "Please enter your coordinates X Y (e.g., enter '0 1' for the coordinate (0,1)): ").split()
+            X = int(X)
+            Y = int(Y)
+            while (X != 0 and X != 1 and X != 2) or (Y != 0 and Y != 1 and Y != 2):
+                print("Incorrect corrdinates entered!")
+                X, Y = input("Please re-enter your coordinates X Y: ").split()
+                X = int(X)
+                Y = int(Y)
+
+            # TODO: Update the board.
+            board.set(X, Y, current_player.name)
+
+            # TODO: Update who is the next turn.
+            winner = board.get_winner()
+            if winner == None:
+                current_player.next_turn()
+                print(board)
+                print("\nNow, it is", current_player.name, "'s turn!")
+            else:
+                print("\n" + winner, "just won the game.")
--- Lab 6 - A Tic-Tac-Toe Game Using Object Oriented Concepts/logic.py
+++ Lab 6 - A Tic-Tac-Toe Game Using Object Oriented Concepts/logic.py
@@ -1,1 +1,90 @@
+# This file is where game logic lives. No input
+# or output happens here. The logic in this file
+# should be unit-testable.
 
+class Board:
+
+    def __init__(self):
+        self.rows = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None]
+        ]
+        self.empty = True
+
+    def __str__(self):
+        s = '\n-------\n'
+        for row in self.rows:
+            for cell in row:
+                s = s + '|'
+                if cell == None:
+                    s = s + ' '
+                else:
+                    s = s + cell
+            s = s + '\n-------\n'
+        return s
+
+    def get(self, x, y):
+        return self.rows[x][y]
+
+    def set(self, x, y, value):
+        self.rows[x][y] = value
+        self.empty = False
+
+    def get_winner(self):
+        """Determines the winner of the given board. Returns 'X', 'O', or None."""
+
+        if self.rows[0][0] == self.rows[0][1] == self.rows[0][2]:
+            if self.rows[0][0] == 'O' or self.rows[0][0] == 'X':
+                return self.rows[0][0]
+            else:
+                return None
+        elif self.rows[1][0] == self.rows[1][1] == self.rows[1][2]:
+            if self.rows[1][0] == 'O' or self.rows[1][0] == 'X':
+                return self.rows[1][0]
+            else:
+                return None
+        elif self.rows[2][0] == self.rows[2][1] == self.rows[2][2]:
+            if self.rows[2][0] == 'O' or self.rows[2][0] == 'X':
+                return self.rows[2][0]
+            else:
+                return None
+        elif self.rows[0][0] == self.rows[1][0] == self.rows[2][0]:
+            if self.rows[0][0] == 'O' or self.rows[0][0] == 'X':
+                return self.rows[0][0]
+            else:
+                return None
+        elif self.rows[0][1] == self.rows[1][1] == self.rows[2][1]:
+            if self.rows[0][1] == 'O' or self.rows[0][1] == 'X':
+                return self.rows[0][1]
+            else:
+                return None
+        elif self.rows[0][2] == self.rows[1][2] == self.rows[2][2]:
+            if self.rows[0][2] == 'O' or self.rows[0][2] == 'X':
+                return self.rows[0][2]
+            else:
+                return None
+        elif self.rows[0][0] == self.rows[1][1] == self.rows[2][2]:
+            if self.rows[0][0] == 'O' or self.rows[0][0] == 'X':
+                return self.rows[0][0]
+            else:
+                return None
+        elif self.rows[0][2] == self.rows[1][1] == self.rows[2][0]:
+            if self.rows[0][2] == 'O' or self.rows[0][2] == 'X':
+                return self.rows[0][2]
+            else:
+                return None
+        else:
+            return None
+
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+
+    def next_turn(self):
+        if self.name == 'O':
+            self.name = 'X'
+        else:
+            self.name = 'O'
