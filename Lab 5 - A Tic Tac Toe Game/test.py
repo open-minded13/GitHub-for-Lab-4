f7f77e56ab8d808e3dbb9a9be45dc8e2084d967d
CW <chiawei1@uw.edu>
2022-11-26 04:35:04 -0800

    Note: Updated the test of another function in logic.py.

--- Lab 5 - A Tic Tac Toe Game/test.py
+++ Lab 5 - A Tic Tac Toe Game/test.py
@@ -4,7 +4,7 @@ import logic
 
 class TestLogic(unittest.TestCase):
 
-    def test_get_winner(self):
+    def test_get_winner_1(self):
         board = [
             ['X', None, 'O'],
             [None, 'X', None],
@@ -12,13 +12,16 @@ class TestLogic(unittest.TestCase):
         ]
         self.assertEqual(logic.get_winner(board), 'X')
 
-    def test_get_winner(self):
+    def test_get_winner_2(self):
         board = [
             ['X', 'O', 'O'],
             ['O', 'X', 'X'],
             ['X', 'O', 'O'],
         ]
         self.assertEqual(logic.get_winner(board), 'Draw')
+
+    def test_other_player(self):
+        self.assertEqual(logic.other_player('O'), 'X')
 
 
 if __name__ == '__main__':
